50713fed3eda6aeb3b9e88142f633f5e588c167d
browniwils <browniwils@gmail.com>
2024-03-02 21:00:26 +0000

    Update jobs endpoint with authentication and authorization

--- backend/api/v1/routes/jobs.py
+++ backend/api/v1/routes/jobs.py
@@ -2,6 +2,9 @@
 """Module for handling jobs API endpoints."""
 from api.v1 import app_views
 from model.job import Job
+from security.auth import AUTH
+from security.auth import authentication_required
+from security.auth import authorization_required
 from utils.endpoint import create_entry
 from utils.endpoint import delete_entry
 from utils.endpoint import get_all
@@ -22,16 +25,22 @@ def get_job(job_id):
 
 
 @app_views.route("/jobs", methods=["POST"])
+@authentication_required
+@authorization_required(AUTH.CREATE)
 def create_job():
     """Create job record in model table."""
     return create_entry(Job)
 
 @app_views.route("/jobs/<job_id>", methods=["PUT"])
+@authentication_required
+@authorization_required(AUTH.UPDATE)
 def update_job(job_id):
     """Update job record in model table."""
     return update_entry(Job, job_id)
 
 @app_views.route("/jobs/<job_id>", methods=["DELETE"])
+@authentication_required
+@authorization_required(AUTH.DELETE)
 def delete_job(job_id):
     """Delete job record in model table."""
     return delete_entry(Job, job_id)
