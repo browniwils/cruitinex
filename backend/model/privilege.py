891d0ae44c29d7278b3b661f2a807aab9027eeac
browniwils <browniwils@gmail.com>
2024-03-02 20:50:21 +0000

    Fix bugs in privilege model

---
+++ backend/model/privilege.py
@@ -0,0 +1,36 @@
+#!/bin/usr/python3
+"""Privilage module for crutinex application."""
+from model import Base
+from model.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy import Table
+from sqlalchemy.orm import relationship
+
+
+class Privilege(BaseModel, Base):
+    """User type model object."""
+    __tablename__ = "privileges"
+    name = Column("name", String(128), nullable=False)
+    roles = relationship("Role", secondary="privilege_role")
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+
+class Role(BaseModel, Base):
+    """Roles table for privilege."""
+    __tablename__ = "roles"
+    name = Column("role", String(10), nullable=False)
+
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+
+privilege_role = Table('privilege_role', Base.metadata,
+    Column('privilege_id', Integer, ForeignKey('privileges.id')),
+    Column('role_id', Integer, ForeignKey('roles.id'))
+)
