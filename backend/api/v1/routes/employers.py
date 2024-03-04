70ee5d617149f99b31e0f9de2adec62ff8072ec7
browniwils <browniwils@gmail.com>
2024-03-04 00:12:16 +0000

    Update employer route

--- backend/api/v1/routes/employers.py
+++ backend/api/v1/routes/employers.py
@@ -1,11 +1,16 @@
 #!/bin/usr/python3
 """Module for handling employers API endpoints."""
 from api.v1 import app_views
-from model.user import User
-from utils.endpoint import create_entry
+from security.auth import AUTH
+from security.auth import authentication_required
+from security.auth import authorization_required
+from utils.endpoint import get_workers
 
 
-@app_views.route("/employers", methods=["POST"])
-def create_employers():
-    """Create user record in model table."""
-    return create_entry(User)
+@app_views.route("/employers")
+@authentication_required
+@authorization_required(AUTH.READ)
+def get_employers():
+    """Retrieve all employers."""
+    return get_workers("Employer")
+
