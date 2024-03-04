3dcb80b9945d1ba61ea687160eae928b6ec598fe
browniwils <browniwils@gmail.com>
2024-03-04 00:11:45 +0000

    Update employee route

--- backend/api/v1/routes/employees.py
+++ backend/api/v1/routes/employees.py
@@ -1,22 +1,16 @@
 #!/bin/usr/python3
 """Module for handling employees API endpoints."""
 from api.v1 import app_views
-from flask import abort
-from flask import jsonify
-from model.user import User
-
+from security.auth import AUTH
+from security.auth import authentication_required
+from security.auth import authorization_required
+from utils.endpoint import get_workers
 
 
 @app_views.route("/employees")
+@authentication_required
+@authorization_required(AUTH.READ)
 def get_employees():
     """Retrieve all employees."""
-
-    return jsonify(), 200
-
-
-@app_views.route("/employees/<employee_id>")
-def get_employee(employee_id):
-    """Retrieve employee with employee id."""
-
-    abort(404)
+    return get_workers("Employee")
 
